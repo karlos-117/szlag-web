fdac64d1ebfe1fa639cfbca7c8ad92f4688565ef
karlos-117 <karlos-117@hotmail.com>
2021-08-24 15:13:18 +0200

    Tutorial part 3 finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
    <Layout pageTitle="Home Page">
      <p>I'm making this by following the gatsby tutorial</p>
+     <StaticImage
+        alt="Szlag logo over a white background"
+        src="../images/szlag-white.jpg"
+      />
    </Layout>
+   
   )
 }
 // Step 3: Export your component
