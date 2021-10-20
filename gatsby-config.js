977bc0d9eaa9a0681d84ffb0070bc7f9a0fcd200
karlos-117 <karlos-117@hotmail.com>
2021-10-20 18:21:55 +0200

    gatsby-transformer-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   plugins: ["gatsby-plugin-gatsby-cloud",
   "gatsby-plugin-image",
   "gatsby-plugin-sharp",
-  "gatsby-transformer-sharp",
   {
     resolve: "gatsby-source-filesystem",
     options: {
